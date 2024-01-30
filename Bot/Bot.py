dd7d44cfddb14fca894b6e0ca983f6b6195ed23a
Saizcharly <83606181+saizchrly@users.noreply.github.com>
2024-01-30 22:54:48 +0100

    llamadas sudo done

--- Bot/Bot.py
+++ Bot/Bot.py
@@ -1,9 +1,11 @@
 import telegram
 import os
+import subprocess
 from telegram.ext import Updater, CommandHandler, MessageHandler, Application, ContextTypes
 
 TELEGRAM = './Configuracion/Bot_telegram.txt'
 HELP='./Configuracion/Help_config.txt'
+SUDO='./Configuracion/Sudo_config.txt'
 
 class Bot:
     """*+
@@ -47,7 +49,6 @@ async def help(update: telegram.Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text='WIP')
 
 def llamadaSistema(entrada):
-    print(1)
     salida = "" # Creamos variable vacia
     f = os.popen(entrada) # Llamada al sistema
     for i in f.readlines(): # Leemos caracter a caracter sobre la linea devuelta por la llamada al sistema
@@ -56,6 +57,16 @@ def llamadaSistema(entrada):
 
     return salida # Devolvemos la respuesta al comando ejecutado
 
+def llamadasSistemaSudo(comando: str):
+    Sudocomando='sudo '+ comando
+    contrasena= ''
+    with open(SUDO, 'r') as f:
+            for line in f.readlines():
+                contrasena=line.strip('\n')
+                
+    proc = subprocess.Popen(comando, stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    proc.stdin.write(contrasena.encode('utf-8'))
+    proc.stdin.close()
 
 async def ls(update: telegram.Update, context: ContextTypes.DEFAULT_TYPE,):
 	
