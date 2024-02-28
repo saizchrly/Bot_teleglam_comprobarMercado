04179bcca3102c09d1e35cb66fb22862a7977646
Saizcharly <83606181+saizchrly@users.noreply.github.com>
2024-02-28 19:36:59 +0100

    Funciona
    Los cambios realizados hacen que ya funcione ;)

--- src/basicos/Comprobarconfiguracion.py
+++ src/basicos/Comprobarconfiguracion.py
@@ -2,14 +2,14 @@ import os
 
 TELEGRAM = './src/Configuracion/Bot_telegram.txt'
 HELP='./src/Configuracion/Help_config.txt'
-ACC='./src/Configuracion/Aciones_config.txt'
+ACC='./src/Configuracion/Acciones_config.txt'
 
 def comprobarConfig():
     if not os.path.exists(TELEGRAM):
         open(TELEGRAM, 'w').close()
     if not os.path.exists(HELP):
         with open(HELP, 'w') as file:
-            file.write('/start -> Este comando tiene el proposito de saber si el bot esta operativo o no.\n/help -> Comando usado para mostrar el menú de ayuda al usuario, el menú actual.\n/reboot contraseña -> Este comando tiene el proposito de reinicial el dispositivo donde se encuentra alojado nuesto bot\n/acciones -> Este comando mostrara las acciones que vamos a comprobar\n/acionesAdd Acion1 Acion2 -> Este comando añadirá acciones a la lista\n/acionesDel Acion1 Acion2 -> Este comando borrará acciones de la lista\n/sendAciones -> Este comandopermite mandar un documento .txt con los precios de las acciones')
+            file.write('/start -> Este comando tiene el proposito de saber si el bot esta operativo o no.\n/help -> Comando usado para mostrar el menú de ayuda al usuario, el menú actual.\n/reboot contraseña -> Este comando tiene el proposito de reinicial el dispositivo donde se encuentra alojado nuesto bot\n/acciones -> Este comando mostrara las acciones que vamos a comprobar\n/accionesAdd Acion1 Acion2 -> Este comando añadirá acciones a la lista\n/accionesDel Acion1 Acion2 -> Este comando borrará acciones de la lista\n/sendAcciones -> Este comandopermite mandar un documento .txt con los precios de las acciones')
         file.close()
     if not os.path.exists(ACC):
         open(ACC, 'w').close()
