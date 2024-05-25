e09fe85ec52aa4fd4a77a8d325c760c7098d1ed0
Saizcharly <83606181+saizchrly@users.noreply.github.com>
2024-05-25 18:10:57 +0200

    #6 Comentarios en la clase Bot

--- src/Bot/Bot.py
+++ src/Bot/Bot.py
@@ -12,12 +12,27 @@ SEND='./src/Configuracion/preciosFinales.txt'
 class Bot:
     
     def __init__(self):
+        """*+
+        Constructor de la clase Bot, en el que se inicializa el bot de telegram
+        """
         self.set_bot(TELEGRAM)
 
-    def set_bot(self, path):
+    def set_bot(self, path: str):
+        """*+
+        Funcion set_bot, en la que se inicializa el bot de telegram
+
+        Args:
+            path (str): ruta del fichero donde se encuentra el token del bot
+        """
         self.bot =Ficheros.leerLineas(path)[0]
                 
-    def get_bot(self):
+    def get_bot(self) -> str:
+        """*+
+        Funcion get_bot, en la que se devuelve el bot de telegram
+
+        Returns:
+            str : token del bot
+        """
         return self.bot
         
     @staticmethod
@@ -33,6 +48,13 @@ class Bot:
 
     @staticmethod
     async def help(update: telegram.Update, context: ContextTypes.DEFAULT_TYPE):
+        """*+
+        Funcion help, con la que se muestra la ayuda del bot
+
+        Args:
+            update (telegram.Update): update del bot
+            context (ContextTypes.DEFAULT_TYPE): contexto del bot
+        """
         texto = ''
         Lineas = Ficheros.leerLineas(HELP)
         for line in Lineas:
@@ -42,17 +64,38 @@ class Bot:
 
     @staticmethod
     async def reboot(update: telegram.Update, context: ContextTypes.DEFAULT_TYPE):
+        """*+
+        Funcion reboot, con la que se reinicia el bot
+
+        Args:
+            update (telegram.Update): update del bot
+            context (ContextTypes.DEFAULT_TYPE): contexto del bot
+        """
         args = context.args  # ACEDEMOS A LOS ARGUMENTOS IMPORTANTE
         a = LlamadasSistema.llamadaSistemaSudo('reboot', args[0])  # Llamada al sistema con sudo
         await context.bot.send_message(chat_id=update.effective_chat.id, text='Reboot, realizado con exito.\nPara confirmar que el bot vuelve a estar operativo use /start')
 
     @staticmethod
     async def acciones(update: telegram.Update, context: ContextTypes.DEFAULT_TYPE):
+        """*+
+        Funcion acciones, con la que se muestran las acciones que se van a comprobar
+
+        Args:
+            update (telegram.Update): update del bot
+            context (ContextTypes.DEFAULT_TYPE): contexto del bot
+        """
         texto = Ficheros.leerAcciones(ACC)
         await context.bot.send_message(chat_id=update.effective_chat.id, text=texto)
 
     @staticmethod
     async def addAcciones(update: telegram.Update, context: ContextTypes.DEFAULT_TYPE):
+        """*+
+        Funcion addAcciones, con la que se añaden acciones a la lista de acciones a comprobar
+
+        Args:
+            update (telegram.Update): update del bot
+            context (ContextTypes.DEFAULT_TYPE): contexto del bot
+        """
         args = context.args  # ACEDEMOS A LOS ARGUMENTOS IMPORTANTE
         acciones = Ficheros.leerLineas(ACC)
         
@@ -68,6 +111,13 @@ class Bot:
 
     @staticmethod
     async def delAcciones(update: telegram.Update, context: ContextTypes.DEFAULT_TYPE):
+        """*+
+        Funcion delAcciones, con la que se eliminan acciones de la lista de acciones a comprobar
+
+        Args:
+            update (telegram.Update): update del bot
+            context (ContextTypes.DEFAULT_TYPE): contexto del bot
+        """
         args = context.args
         acciones = Ficheros.leerLineas(ACC)
 
@@ -84,6 +134,13 @@ class Bot:
 
     @staticmethod
     async def SendAcciones(update: telegram.Update, context: ContextTypes.DEFAULT_TYPE):
+        """*+
+        Funcion SendAcciones, con la que se envian las acciones a comprobar
+
+        Args:
+            update (telegram.Update): update del bot
+            context (ContextTypes.DEFAULT_TYPE): contexto del bot
+        """
         lectorMercado.obtener_precios_acciones(lectorMercado(Ficheros.leerLineas(ACC)))
 
         with open(SEND, 'rb') as file:
