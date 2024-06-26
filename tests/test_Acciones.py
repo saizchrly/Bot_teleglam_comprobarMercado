a6f96718f1c18ffac9c3038290db6f3fd32b73c0
Saizcharly <83606181+saizchrly@users.noreply.github.com>
2024-06-26 20:02:52 +0200

    #5 Añadimos los test de la clase Acciones

---
+++ tests/test_Acciones.py
@@ -0,0 +1,46 @@
+import unittest
+from src.Acciones.Acciones import Acciones
+
+class TestAcciones(unittest.TestCase):
+    def setUp(self):
+        self.accion = Acciones("AAPL")
+        self.accion.set_valor_inicial(100)
+        self.accion.set_valor_final(120)
+        self.accion.set_diferencia(20)
+
+    def test_get_nombre(self):
+        self.assertEqual(self.accion.get_nombre(), "AAPL")
+
+    def test_get_valor_inicial(self):
+        self.assertEqual(self.accion.get_valor_inicial(), 100)
+
+    def test_get_valor_final(self):
+        self.assertEqual(self.accion.get_valor_final(), 120)
+
+    def test_get_diferencia(self):
+        self.assertEqual(self.accion.get_diferencia(), 20)
+        
+    def test_set_nombre(self):
+        self.accion.set_nombre("KO")
+        self.assertEqual(self.accion.get_nombre(), "KO")
+
+    def test_set_valor_inicial(self):
+        self.accion.set_valor_inicial(150)
+        self.assertEqual(self.accion.get_valor_inicial(), 150)
+
+    def test_set_valor_final(self):
+        self.accion.set_valor_final(200)
+        self.assertEqual(self.accion.get_valor_final(), 200)
+    
+    def test_set_diferencia(self):
+        self.accion.set_diferencia(50)
+        self.assertEqual(self.accion.get_diferencia(), 50)
+        
+    def test_calcular_diferencia(self):
+        self.accion.calcular_diferencia()
+        self.assertEqual(self.accion.get_diferencia(), 20)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
